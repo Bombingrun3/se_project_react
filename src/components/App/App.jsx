befee05feacb4b8ffe3e4f81a17b0f876641ed1a
Ryan B <bombingrun3@gmail.com>
2025-04-02 20:40:16 -0500

    Added Logo and Avatar and set default font styling to entire app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Header from "../Header/Header";
 
 function App() {
-  return <div className="app">Hello World</div>;
+  return (
+    <div className="app">
+      <div className="app__content"></div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,27 @@
+import "./Header.css";
+import logo from "../../assets/logo.svg";
+import avatar from "../../assets/avatar.svg";
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="header__logo" alt="logo" src={logo}></img>
+      <p className="header__date-location">April 01, Evansville</p>
+      <button className="header__add-clothes-button">+ Add Clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Username</p>
+        <img
+          className="header__avatar"
+          alt="Terrence Tegegne"
+          src={avatar}
+        ></img>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
+
 // A logo
 // The current date that can be generated using the Date() object: javascript const currentDate = new Date().toLocaleString('default', { month: 'long', day: 'numeric' });
 // The current location (see Section 5 for details)
