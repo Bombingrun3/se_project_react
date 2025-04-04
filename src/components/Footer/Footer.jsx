a3805d768b70b8ef7b499c83bc1e647ff4010437
Ryan B <bombingrun3@gmail.com>
2025-04-04 14:00:38 -0400

    Added Footer and Styled

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,2 +1,15 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__author">Developed by Ryan Bommarito</p>
+      <p className="footer__date">2025</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
 // A component that renders meta info about the app: the copyright
 // and the production year. It consists of basic text.
