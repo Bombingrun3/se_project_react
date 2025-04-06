69aefd65569fc76d7982734f98110276595675f3
Ryan B <bombingrun3@gmail.com>
2025-04-05 21:44:48 -0400

    Added ModalWithForm and started to style

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,7 +1,57 @@
 import "./ModalWithForm.css";
+import close from "../../assets/close-button.svg";
 
 function ModalWithForm() {
-  return <section className="modal"></section>;
+  return (
+    <div className="modal">
+      <form className="modal__form">
+        <h2 className="modal__title">New garment</h2>
+        <button className="modal__close" type="button" src={close}></button>
+        <label htmlFor="name" className="modal__label">
+          Name{" "}
+          <input
+            className="modal__input"
+            type="text"
+            id="name"
+            placeholder="Name"
+          ></input>
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image{" "}
+          <input
+            className="modal__input"
+            type="url"
+            id="imageUrl"
+            placeholder="Image URL"
+          ></input>
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input className="modal__radio-button" type="radio" id="hot" />
+            Hot
+          </label>
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input className="modal__radio-button" type="radio" id="warm" />
+            Warm
+          </label>
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input className="modal__radio-button" type="radio" id="cold" />
+            Cold
+          </label>
+        </fieldset>
+        <button className="modal__submit-button" type="button">
+          Add garment
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default ModalWithForm;
