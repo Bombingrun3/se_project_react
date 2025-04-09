014508d25eaaae2890b3725010e17b7792557d1f
Ryan B <bombingrun3@gmail.com>
2025-04-08 23:10:05 -0500

    Added WeatherCard Feature that changes based on the locations weather conditons

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,11 +1,30 @@
 import "./WeatherCard.css";
-import sunnyDay from "../../assets/sunny-day.svg";
+import { weatherOptions } from "../../utils/constants";
 
 function WeatherCard({ weatherData }) {
+  if (!weatherData) {
+    return <div>Loading...</div>;
+  }
+  const filteredWeatherOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+
+  const weatherOption = filteredWeatherOptions[0];
+
+  const weatherOptionUrl = filteredWeatherOptions[0]?.url;
+  const weatherOptionCondition = filteredWeatherOptions[0]?.condition;
+
   return (
     <section className="weathercard">
       <p className="weathercard__temp">{`${weatherData.temp.F} F`}</p>
-      <img className="weathercard__image" alt="Sunny Day" src={sunnyDay} />
+      <img
+        className="weathercard__image"
+        alt={weatherOption?.condition}
+        src={weatherOption?.url}
+      />
     </section>
   );
 }
